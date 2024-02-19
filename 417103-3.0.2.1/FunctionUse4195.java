1e148e91d5dc36e65fd1c322712df37aa4ebdd4c
duzhg56980 <guokey1996@gmail.com>
2024-02-19 10:55:31 +0800

    init commit

---
+++ 417103-3.0.2.1/FunctionUse4195.java
@@ -0,0 +1,7 @@
+import org.apache.dubbo.rpc.model.ServiceDescriptor;
+public class FunctionUse4195 {
+public void funcUse() {
+ServiceDescriptor servicedescriptor = new ServiceDescriptor();
+servicedescriptor.lambda$initMethods$3(null,null);
+}
+}
